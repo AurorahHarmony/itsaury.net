c0188f66a5f75cc9df3a48503c5bd8fb2cca05a0
Aurorah Harmony <aurorahHarmony@gmail.com>
2023-07-13 14:22:02 +1200

    Add homepage

--- _config.ts
+++ _config.ts
@@ -1,8 +1,11 @@
 import lume from "lume/mod.ts";
+import imagick from "lume/plugins/imagick.ts";
 import sass from "lume/plugins/sass.ts";
 
 const site = lume();
 
-site.use(sass());
+site.use(sass()).use(imagick());
+
+site.copy("static", ".");
 
 export default site;
