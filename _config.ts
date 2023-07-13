92d212dba93a738b111d2f9ce55eaae957f3c6bc
Aurorah Harmony <aurorahHarmony@gmail.com>
2023-07-13 17:46:29 +1200

    Add gradient generator

--- _config.ts
+++ _config.ts
@@ -6,6 +6,19 @@ const site = lume();
 
 site.use(sass()).use(imagick());
 
+/** Generates a gradient for project cards */
+site.filter(
+  "formatGradient",
+  (colorStops: { percent: string; color: string }[]) => {
+    if (Array.isArray(colorStops) && colorStops.length > 0) {
+      const stops = colorStops.map((stop) => `${stop.color} ${stop.percent}%`);
+      return `linear-gradient(59deg, ${stops.join(",")})`;
+    } else {
+      return "linear-gradient(59deg, rgba(99,24,195,1) 0%, rgba(37,41,139,1) 100%)";
+    }
+  }
+);
+
 site.copy("static", ".");
 
 export default site;
